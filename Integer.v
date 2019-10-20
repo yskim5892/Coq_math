d817f5e9bbecfab9fa433dd84c991b720b4e208d
KimYoonsung <yskim227@snu.ac.kr>
2019-10-21 02:30:54 +0900

    Simplify properties of z_le and z_lt.
     * using propositions from Ordering.

--- Integer.v
+++ Integer.v
@@ -1,9 +1,13 @@
+Require Import Logic_utils.
+Require Import Relation.
+Require Import Ordering.
 Require Import NatProps.
+
 Inductive Z : Type :=
   | z ( n m : nat).
 
-Axiom z_eq : forall (n1 n2 m1 m2 : nat),
-n1 + m2 = n2 + m1 -> z n1 n2 = z m1 m2.
+Axiom z_eq : forall (n0 n1 m0 m1 : nat),
+n0 + m1 = n1 + m0 <-> z n0 m0 = z n1 m1.
 
 Definition z_plus (z1 : Z)(z2 : Z) : Z :=
   match z1 with 
@@ -106,6 +110,130 @@ rewrite <- n_distributive with (n:=k1). rewrite n_mul_comm with (n:=k1).
 rewrite n_plus_comm with (n:=l*l0). reflexivity.
 
 rewrite H. rewrite H with (k:=n)(l:=m). reflexivity.
+Qed.
+
+
+(* inequality *)
+
+Definition z_le : Relation Z Z :=
+fun p => match p with
+| (a, b) => ( match a with
+  | z n0 m0 => match b with
+    | z n1 m1 => n0 + m1 <= n1 + m0
+    end
+  end)
+end.
+
+Definition z_lt : Relation Z Z :=
+fun p => match p with
+| (a, b) => ( match a with
+  | z n0 m0 => match b with
+    | z n1 m1 => n0 + m1 < n1 + m0
+    end
+  end)
+end.
+
+Definition z_ge : Relation Z Z :=
+fun p => match p with
+| (a, b) => z_le (b, a)
+end.
+
+Definition z_gt : Relation Z Z :=
+fun p => match p with
+| (a, b) => z_lt (b, a)
+end.
+
+Theorem z_le_reflexive : forall (a : Z),
+  z_le(a, a).
+Proof.
+destruct a. simpl. apply le_n.
+Qed.
+
+Theorem z_le_transitive : forall (a b c : Z),
+  z_le(a, b) /\ z_le(b, c) -> z_le(a, c).
+Proof.
+destruct a, b, c. simpl. intros.
+apply proj1 in H as H1. apply proj2 in H as H2.
+assert (H3 : (n + m0 <= n0 + m) /\ (n0 + m1 <= n1 + m0)).
+{ apply conj. apply H1. apply H2. }
+apply n_le_sum in H3. 
+rewrite n_plus_comm with (n:=n0) in H3. rewrite n_plus_comm with (n:=n0) in H3.
+rewrite n_plus_comm with (n:=m+n0) in H3. rewrite n_plus_assoc in H3.
+rewrite n_plus_assoc with (k:=n0) in H3. 
+apply n_le_plus in H3.
+rewrite <- n_plus_assoc in H3. rewrite <- n_plus_assoc in H3.
+rewrite n_plus_comm with (n:=m0) in H3. rewrite n_plus_comm with (n:=m0) in H3.
+rewrite n_plus_assoc in H3. rewrite n_plus_assoc in H3.
+apply n_le_plus in H3.
+apply H3.
+Qed.
+
+Theorem z_le_antisymmetric : forall (a b : Z),
+  z_le(a, b) /\ z_le(b, a) -> a = b.
+Proof.
+destruct a, b. simpl. intros.
+apply n_le_antisymmetric in H. apply z_eq.
+apply H.
+Qed.
+
+Theorem z_le_total_ordering : forall (a b : Z),
+  z_le(a, b) \/ z_le(b, a).
+Proof.
+intros. destruct a, b. simpl.
+apply n_le_total_partial_ordering.
+Qed.
+
+Theorem z_le_total_partial_ordering : total_partial_ordering z_le.
+Proof.
+unfold total_partial_ordering. apply conj.
+- unfold partial_ordering. apply conj.
+  + unfold reflexive. apply z_le_reflexive.
+  + apply conj.
+    * unfold antisymmetric. apply z_le_antisymmetric.
+    * unfold transitive. apply z_le_transitive.
+- apply z_le_total_ordering.
+Qed.
+
+Theorem z_lt_is_strict_z_le : z_lt = partial_to_strict z_le.
+Proof.
+assert (nl : forall (n m: nat), n_lt (n, m) <-> partial_to_strict n_le (n, m)).
+{ apply Relation_eq. apply n_lt_is_strict_n_le. }
+unfold partial_to_strict in nl.
+
+apply Relation_eq. intros. unfold partial_to_strict.
+destruct a, b.
+unfold z_lt, z_le.
+assert (zneq : z n m <> z n0 m0 <-> n + m0 <> n0 + m).
+{ apply not_iff_compat. apply iff_sym. apply z_eq. }
+apply and_iff_compat_l with (A:=n+m0<=n0+m) in zneq.
+apply iff_iff_compat_l with (A:=n+m0<n0+m) in zneq.
+apply zneq. apply nl.
+Qed.
+
+Theorem z_le_is_partial_z_lt : z_le = strict_to_partial z_lt.
+Proof.
+rewrite z_lt_is_strict_z_le. apply eq_sym.
+apply partial_to_strict_to_partial_identity.
+apply z_le_total_partial_ordering.
+Qed.
+
+Theorem z_lt_total_strict_ordering : total_strict_ordering z_lt.
+Proof.
+rewrite z_lt_is_strict_z_le.
+apply partial_to_strict_preserves_totality.
+apply z_le_total_partial_ordering.
+Qed.
+
+Theorem z_le_plus : forall (a b c : Z),
+  z_le(a, b) -> z_le (z_plus(a, c)) (z_plus(b, c)).
+Proof.
+destruct a, b, c. simpl. intros.
+rewrite n_plus_assoc. rewrite n_plus_assoc.
+apply n_le_plus.
+rewrite <- n_plus_assoc. rewrite <- n_plus_assoc.
+rewrite n_plus_comm with (n:=n1). rewrite n_plus_comm with (n:=n1).
+rewrite n_plus_assoc. rewrite n_plus_assoc.
+apply n_le_plus. apply H.
 Qed.
 
 
